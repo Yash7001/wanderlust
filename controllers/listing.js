e98a1fa9b93279a1576737b837f150fed8655267
Yash Prajapati <yashprajapati1007@gmail.com>
2023-12-30 16:37:26 +0530

    new Experiments 3

--- controllers/listing.js
+++ controllers/listing.js
@@ -80,10 +80,19 @@ module.exports.updateListing = async (req, res) => {
   res.redirect(`/listings/${id}`);
 };
 
-module.exports.destroyListing = async (req, res) => {
-  let { id } = req.params;
-  let deletedLisiting = await Listing.findByIdAndDelete(id);
-  console.log(deletedLisiting);
-  req.flash("success", "Listing Deleted");
-  res.redirect("/listings");
-};
+// module.exports.destroyListing = async (req, res) => {
+//   let { id } = req.params;
+//   let deletedLisiting = await Listing.findByIdAndDelete(id);
+//   console.log(deletedLisiting);
+//   req.flash("success", "Listing Deleted");
+//   res.redirect("/listings");
+// };
+
+// Destroy route (deletes listing) 
+module.exports.destroyListing = async (req,res)=>{ 
+  let {id}= req.params; 
+  let deletedListing = await Listing.findByIdAndDelete(id); 
+  console.log(deletedListing); 
+  req.flash("success","Listing Deleted !!") 
+  res.redirect("/listings") 
+}
